53a95e90981f4affaadbc650aee62399739b9c6e
jiteshwar <36121865+jiteshwar@users.noreply.github.com>
2021-04-04 05:10:52 -0500

    Add files via upload

---
+++ Week3/assignment_03.2_AnjaleJiteshwar.R
@@ -0,0 +1,92 @@
+# Assignment: ASSIGNMENT 3.2
+# Name: Anjale, Jiteshwar
+# Date: 2021-03-30
+#American Community Survey Exercise
+
+## Load the ggplot2 package
+library(ggplot2)
+theme_set(theme_minimal())
+
+## Load the pastecs package
+library(pastecs)
+
+## Set the working directory to the root of your DSC 520 directory
+setwd('C:/Users/anjal/OneDrive/Desktop/MS/DSC520/dsc520')
+
+## Load the `data/acs-14-1yr-s0201.csv` to
+acs_df <- read.csv("data/acs-14-1yr-s0201.csv")
+
+
+## i.What are the elements in your data (including the categories and data types)?
+summary(acs_df)
+
+
+
+## ii.Please provide the output from the following functions: str(); nrow(); ncol()
+str(acs_df)
+nrow(acs_df)
+ncol(acs_df)
+
+## iii.Create a Histogram of the HSDegree variable using the ggplot2 package.
+ggplot(acs_df, aes(x=HSDegree)) + geom_histogram()
+
+## 1.Set a bin size for the Histogram.
+ggplot(acs_df, aes(HSDegree)) + geom_histogram(bins = 10)
+
+## 2.Include a Title and appropriate X/Y axis labels on your Histogram Plot.
+ggplot(acs_df, aes(HSDegree)) + geom_histogram(bins = 10) + ggtitle("HSDegree vs Frequnecy") + xlab("HSDegree(%)") +ylab("Frequnecy") 
+
+## vi.Answer the following questions based on the Histogram produced:
+
+## 1.Based on what you see in this histogram, is the data distribution unimodal?
+## As the probability distribution in given histogram has a single peak, the data distribution unimodal.
+
+## 2.Is it approximately symmetrical?
+##  The distribution is not symmetrical
+
+## 3.Is it approximately bell-shaped?
+## The distribution is not bell-shaped
+
+## 4.Is it approximately normal?
+## The distribution is not normal.
+
+## 5.If not normal, is the distribution skewed? If so, in which direction?
+## The distribution skewed in left (hence it is negatively Skewed)
+
+## 6.Include a normal curve to the Histogram that you plotted.
+ggplot(acs_df, aes(x=HSDegree)) + geom_histogram(aes(y = ..density..),bins = 10)+ stat_function(fun = dnorm, colour = "red",args = list(mean = mean(acs_df$HSDegree, na.rm = TRUE), sd = sd(acs_df$HSDegree, na.rm = TRUE))) + ggtitle("HSDegree vs Frequnecy") + xlab("HSDegree(%)") +ylab("Frequnecy") 
+
+## 7.Explain whether a normal distribution can accurately be used as a model for this data.
+## A normal distribution can not accurately be used as a model for this data 
+##because the histogram does not have shape of normal curve and 
+##The histogram shows that the distribution is negatively skewed. 
+
+## V.Create a Probability Plot of the HSDegree variable.
+ggplot(acs_df, aes(sample=HSDegree)) + stat_qq() + stat_qq_line()
+
+
+## vi.Answer the following questions based on the Probability Plot:
+
+## 1.Based on what you see in this probability plot, is the distribution approximately normal? Explain how you know.
+## The straight line in this plot represents
+## a normal distribution, and the points represent the observed residuals. Therefore, in
+## a perfectly normally distributed data set, all points will lie on the line.
+## As the points are not on line, it is not normal distribution.
+
+##2.If not normal, is the distribution skewed? If so, in which direction? Explain how you know.
+##In this plot, the lower end of QQ plot deviates from straight line 
+##then we can clearly say that the distribution has a longer tail to its left or simply it is left-skewed (or negatively skewed) 
+
+## vii. Now that you have looked at this data visually for normality, 
+##you will now quantify normality with numbers using the stat.desc() function. 
+##Include a screen capture of the results produced.
+stat.desc(acs_df, basic=TRUE, desc=TRUE, norm=FALSE, p=0.95)
+
+
+stat.desc(acs_df["HSDegree"], basic=FALSE, desc=TRUE, norm=TRUE)
+## viii. In several sentences provide an explanation of the result produced for 
+##skew, kurtosis, and z-scores. In addition, explain how a change in the sample size may change your explanation?
+## The negative value for skewness indicates that the data are skewed left.
+## The positive value of of kurtosis indicates a "heavy-tailed" distribution.
+## Z score for skew (skew.2SE) and Z score for kurtosis(kurt.2SE) are greater than 1 and hence both skew and kurtosis are significant.
+##Large samples will give rise to small standard errors and so when sample sizes are big, significant values arise from even small deviations from normality. Also for larger sample size we need to use visualizations for data analysis.
